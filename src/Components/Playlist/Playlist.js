1c656bb8dfda8b1c20d9a070334323726c68046a
unknown <Aaron>
2018-01-09 14:44:26 -0800

    upload files

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,6 +3,15 @@ import './Playlist.css';
 import TrackList from '../TrackList/TrackList';
 class Playlist extends React.Component {
   
+  
+  constructor(props) {
+	  super(props);
+	  
+	  this.handleNameChange = this.handleNameChange.bind(this);
+  }
+  
+  
+  
   handleNameChange(event) {
 	  
 	this.props.onNameChange(event.target.value);
