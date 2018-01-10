0d65d86ab30057767ee32a7ef97d273e2deb094f
unknown <Aaron>
2018-01-09 16:17:20 -0800

    upload files

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -28,13 +28,10 @@ class Track extends React.Component {
     return (
     <div className="Track">
 	  <div className="Track-information">
-		/*track name will go here */
 		<h3>{this.props.track.name} </h3>
-		/* track artist will go here--> |  track album will go here */
 		<p>{this.props.track.artist} | {this.props.track.album} </p>
 	  </div>
-	  /* + or - will go here */
-	  <a className="Track-action">{this.renderAction}</a>
+		this.renderAction	
 	</div>
     );
   }
